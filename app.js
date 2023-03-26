59728393e1ad729bc86ad99498ab965a94df50fa
thekushmevada <kush_20092@ldrp.ac.in>
2023-03-26 14:11:22 +0530

    MongoDB Connection Added

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
+const mongoURL = "mongodb+srv://kushmevada:sigmasquare@cluster0.sffios3.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(mongoURL , {
+  useNewUrlParser : true
+}).then(() => {
+  console.log("MongoDB Connected");
+}).catch((e) => {
+  console.log(e);
+})
+
 app.listen(port, () => {
-    console.log(`Server Started at port ${port}`);
-  });
+  console.log(`Server Started at port ${port}`);
+});
+
